5894dbb88e174a62ec7c634d297d200ee3604033
Mo7amed Hany <mo7amed52010@gmail.com>
2026-02-25 02:53:46 +0200

    Refactor text_mode.py for improved functionality

--- text_mode.py
+++ text_mode.py
@@ -12,7 +12,7 @@ def clean (text) :
       return(re.sub(pattern,"",text)) 
 
 def processing_text (file, mode_selection, manual_page_num):
-    client = genai.Client(api_key=st.secrets[GEMINI_API_KEY])
+    client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
     all_text = ""      
     if file is not None :
          with pdfplumber.open(file) as pdf:
@@ -104,6 +104,7 @@ def processing_text (file, mode_selection, manual_page_num):
               st.success("أداء جيد جداً! 👍")
             else:
               st.warning("محاولة جيدة، حاول مرة أخرى لتحسين درجتك. 💪")
+
 
 
 
