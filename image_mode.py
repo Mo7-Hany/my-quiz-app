ce1a7be1b7ff855cbec6010900a0c71a811a4abc
Mo7amed Hany <mo7amed52010@gmail.com>
2026-04-21 15:40:52 +0200

    Update image_mode.py

--- image_mode.py
+++ image_mode.py
@@ -60,7 +60,7 @@ def processing_image(file,mode_selection,manual_page_num,num_questions,difficult
             with st.spinner("جاري توليد الأسئلة من جيميني..."):
                try:
                  response = client.models.generate_content(
-                    model="gemini-3.0-flash", 
+                    model="gemini-2.5-flash", 
                     contents=[
                         my_prompt,
                         types.Part.from_bytes(data=image_bytes, mime_type="image/png")
